1fc608f4bfdf6ec1cc054dafb1f2791c90c0d762
bala-amypo <bala@amypo.in>
2025-12-17 08:41:51 +0000

    User commit

--- src/main/java/com/example/demo/model/VolunteerProfile.java
+++ src/main/java/com/example/demo/model/VolunteerProfile.java
@@ -20,10 +20,23 @@ public class VolunteerProfile{
     public void setVolunteer(String volunteer){
         this.volunteer=volunteer;
     }
-    public String FullName(){
+    public String getFullName(){
         return fullName;
     }
     public void setFullName(String fullName){
         this.fullName=fullName;
+    }
+
+    public String getEmail(){
+         return email;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+    public String getPhone(){
+         return email;
+    }
+    public void setEmail(String email){
+        this.email=email;
     }
 }
