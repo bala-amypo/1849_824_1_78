63357a7d6e9337762ab1ff3094fe3b60e8838c69
bala-amypo <bala@amypo.in>
2025-12-17 08:53:43 +0000

    User commit

--- src/main/java/com/example/demo/model/VolunteerSkillRecord.java
+++ src/main/java/com/example/demo/model/VolunteerSkillRecord.java
@@ -6,4 +6,51 @@ public class VolunteerSkillRecord{
     private String skillLevel;
     private Boolean certified;
     private LocalDateTime updatedAt;
+
+      
+    public Long getId(){
+        return id;
+    }
+    public void setId(String id){
+        this.id=id;
+    }
+    public Long getVolunteerId(){
+        return volunteerId;
+    }
+    public void setVolunteerId(String volunteerId){
+        this.volunteerId=volunteerId;
+    }
+    public String getSkillName(){
+        return skillName;
+    }
+    public void setSkillLevel(String skillLevel){
+        this.skillLevel=skillLevel;
+    }
+
+    public Boolean getCertified(){
+         return email;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+    public String getPhone(){
+         return phone;
+    }
+    public void setPhone(String phone){
+        this.phone=phone;
+    }
+
+    public String getAvailabilityStatus(){
+         return availabilityStatus;
+    }
+    public void setAvailabilityStatus(String availabilityStatus){
+        this.availabilityStatus=availabilityStatus;
+    }
+
+    public LocalDateTime getCreatedAt(){
+         return email;
+    }
+    public void setEmail(LocalDateTime createdAt){
+        this.createdAt=createdAt;
+    }
 }
