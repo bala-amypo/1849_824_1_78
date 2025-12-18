b0fc9f5ee3e24097d438645be0f2f57b2c111de7
bala-amypo <bala@amypo.in>
2025-12-18 05:25:59 +0000

    User commit

--- src/main/java/com/example/demo/model/TaskRecord.java
+++ src/main/java/com/example/demo/model/TaskRecord.java
@@ -17,7 +17,7 @@ public class TaskRecord{
     public Long getId(){
         return id;
     }
-    public void setId(String id){
+    public void setId(Long id){
         this.id=id;
     }
     public String getTaskcode(){
