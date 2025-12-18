f53a06c9202c410beff15e41ae31b83dfb6da726
bala-amypo <bala@amypo.in>
2025-12-18 07:58:18 +0000

    User commit

--- src/main/java/com/example/demo/model/AssignmentEvaluationRecord.java
+++ src/main/java/com/example/demo/model/AssignmentEvaluationRecord.java
@@ -1,55 +1,55 @@
-package com.example.demo.model;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import java.time.LocalDateTime;
-@Entity
-public class AssignmentEvaluationRecord{
-    @Id
-    private Long id;
-    private Long assignmentId;
-    private Integer rating;
-    private String feedback;
-    private LocalDateTime evaluatedAt;
+// package com.example.demo.model;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.Id;
+// import java.time.LocalDateTime;
+// @Entity
+// public class AssignmentEvaluationRecord{
+//     @Id
+//     private Long id;
+//     private Long assignmentId;
+//     private Integer rating;
+//     private String feedback;
+//     private LocalDateTime evaluatedAt;
       
-    public Long getId(){
-        return id;
-    }
-    public void setId(Long id){
-        this.id=id;
-    }
-    public Long getAssignmentId(){
-        return assignmentId;
-    }
-    public void setAssignmentId(Long assignmentId){
-        this.assignmentId=assignmentId;
-    }
-    public Integer getRating(){
-        return rating;
-    }
-    public void setRating(Integer rating){
-        this.rating=rating;
-    }
+//     public Long getId(){
+//         return id;
+//     }
+//     public void setId(Long id){
+//         this.id=id;
+//     }
+//     public Long getAssignmentId(){
+//         return assignmentId;
+//     }
+//     public void setAssignmentId(Long assignmentId){
+//         this.assignmentId=assignmentId;
+//     }
+//     public Integer getRating(){
+//         return rating;
+//     }
+//     public void setRating(Integer rating){
+//         this.rating=rating;
+//     }
 
-    public String getFeedback(){
-         return feedback;
-    }
-    public void setFeedback(String feedback){
-        this.feedback=feedback;
-    }
-    public LocalDateTime getEvaluatedAt(){
-         return evaluatedAt;
-    }
-    public void setEvaluatedAt(LocalDateTime evaluatedAt){
-        this.evaluatedAt=evaluatedAt;
-    }
-    public AssignmentEvaluationRecord(Long id,Long assignmentId,Integer rating,String feedback,LocalDateTime evaluatedAt){
-    this.id=id;
-    this.assignmentId=assignmentId;
-    this.rating=rating;
-    this.feedback=feedback;
-    this.evaluatedAt=evaluatedAt;
-}
-public AssignmentEvaluationRecord(){
+//     public String getFeedback(){
+//          return feedback;
+//     }
+//     public void setFeedback(String feedback){
+//         this.feedback=feedback;
+//     }
+//     public LocalDateTime getEvaluatedAt(){
+//          return evaluatedAt;
+//     }
+//     public void setEvaluatedAt(LocalDateTime evaluatedAt){
+//         this.evaluatedAt=evaluatedAt;
+//     }
+//     public AssignmentEvaluationRecord(Long id,Long assignmentId,Integer rating,String feedback,LocalDateTime evaluatedAt){
+//     this.id=id;
+//     this.assignmentId=assignmentId;
+//     this.rating=rating;
+//     this.feedback=feedback;
+//     this.evaluatedAt=evaluatedAt;
+// }
+// public AssignmentEvaluationRecord(){
 
-}
-}
+// }
+// }
