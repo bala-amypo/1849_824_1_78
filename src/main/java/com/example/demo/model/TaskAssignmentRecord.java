7fb50565b210eb1fd5b3cdc8b82438750f3d2f8d
bala-amypo <bala@amypo.in>
2025-12-18 04:34:46 +0000

    User commit

--- src/main/java/com/example/demo/model/TaskAssignmentRecord.java
+++ src/main/java/com/example/demo/model/TaskAssignmentRecord.java
@@ -1,63 +1,63 @@
-// package.com.example.demo;
-// import jakarta.persistence.Entity;
-// import jakarta.persistence.Id;
-// @Entity
-// public class TaskAssignmentRecord{
-//     @Id
-//     private Long id;
-//     private Long taskId;
-//     private Long volunteerId;
-//     private LocalDateTime assignedAt;
-//     private String status;
-//     private LocalDateTime createdAt;
+package.com.example.demo;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+@Entity
+public class TaskAssignmentRecord{
+    @Id
+    private Long id;
+    private Long taskId;
+    private Long volunteerId;
+    private LocalDateTime assignedAt;
+    private String status;
+    private LocalDateTime createdAt;
       
-//     public Long getId(){
-//         return id;
-//     }
-//     public void setId(String id){
-//         this.id=id;
-//     }
-//     public Long getTaskId(){
-//         return taskId;
-//     }
-//     public void setTaskId(Long taskId){
-//         this.taskId=taskId;
-//     }
-//     public Long getVolunteerId(){
-//         return volunteerId;
-//     }
-//     public void setVolunteerId(Long volunteerId){
-//         this.volunteerId=volunteerId;
-//     }
+    public Long getId(){
+        return id;
+    }
+    public void setId(String id){
+        this.id=id;
+    }
+    public Long getTaskId(){
+        return taskId;
+    }
+    public void setTaskId(Long taskId){
+        this.taskId=taskId;
+    }
+    public Long getVolunteerId(){
+        return volunteerId;
+    }
+    public void setVolunteerId(Long volunteerId){
+        this.volunteerId=volunteerId;
+    }
 
-//     public LocalDateTime getAssignedAt(){
-//          return LocalDateTime;
-//     }
-//     public void setAssignedAt(LocalDateTime assignedAt){
-//         this.assignedAt=assignedAt;
-//     }
-//     public String getStatus(){
-//          return status;
-//     }
-//     public void setStatus(String status){
-//         this.status=status;
-//     }
+    public LocalDateTime getAssignedAt(){
+         return LocalDateTime;
+    }
+    public void setAssignedAt(LocalDateTime assignedAt){
+        this.assignedAt=assignedAt;
+    }
+    public String getStatus(){
+         return status;
+    }
+    public void setStatus(String status){
+        this.status=status;
+    }
 
-//     public LocalDateTime getCreatedAt(){
-//          return createdAt;
-//     }
-//     public void setCreatedAt(LocalDateTime createdAt){
-//         this.createdAt=createdAt;
-//     }
-//     public TaskAssignmentRecord(Long id,Long taskId,Long volunteerId,LocalDateTime assignedAt,String status,LocalDateTime createdAt){
-//     this.id=id;
-//     this.taskId=taskId;
-//     this.volunteerId=volunteerId;
-//     this.assignedAt=assignedAt;
-//     this.status=status;
-//     this.createdAt=createdAt;
-// }
-// public TaskAssignmentRecord(){
+    public LocalDateTime getCreatedAt(){
+         return createdAt;
+    }
+    public void setCreatedAt(LocalDateTime createdAt){
+        this.createdAt=createdAt;
+    }
+    public TaskAssignmentRecord(Long id,Long taskId,Long volunteerId,LocalDateTime assignedAt,String status,LocalDateTime createdAt){
+    this.id=id;
+    this.taskId=taskId;
+    this.volunteerId=volunteerId;
+    this.assignedAt=assignedAt;
+    this.status=status;
+    this.createdAt=createdAt;
+}
+public TaskAssignmentRecord(){
 
-// }
-// }
+}
+
