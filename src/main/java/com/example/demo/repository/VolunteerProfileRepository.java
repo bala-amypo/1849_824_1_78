0fd223aaf7a96b2602c022f830de38599fd8f831
bala-amypo <bala@amypo.in>
2025-12-18 08:56:56 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/VolunteerProfileRepository.java
@@ -0,0 +1,6 @@
+package com.example.demo.repository;
+import org.springframework.data.jap.repository.JpaRepository;
+import com.example.demo.Entityclass.VolunteerProfileRepository;
+public interface  VolunteerProfileRepository extends JpaRepository<  VolunteerProfileRepository,Long>{
+    
+}
