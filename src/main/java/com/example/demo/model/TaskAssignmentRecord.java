928870482a0b268d0b352dfc1cf19e33ba81bb7c
bala-amypo <bala@amypo.in>
2025-12-18 04:55:23 +0000

    User commit

--- src/main/java/com/example/demo/model/TaskAssignmentRecord.java
+++ src/main/java/com/example/demo/model/TaskAssignmentRecord.java
@@ -1,7 +1,7 @@
 package com.example.demo.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.LocalDateTime;
+import java.persistence.LocalDateTime;
 @Entity
 public class TaskAssignmentRecord{
     @Id
