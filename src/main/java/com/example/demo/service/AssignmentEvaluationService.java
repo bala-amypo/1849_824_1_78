9bb3fb09d483e7ba269c263e87d9f8de00379a02
bala-amypo <bala@amypo.in>
2025-12-19 03:51:52 +0000

    User commit

--- src/main/java/com/example/demo/service/AssignmentEvaluationService.java
+++ src/main/java/com/example/demo/service/AssignmentEvaluationService.java
@@ -1,12 +1,20 @@
-package com.example.demo.service;
-import jakarta.persistence.Service;
-import com.example.demo.model.AssignmentEvaluation;
+package com.example.demo.service.impl;
+import com.example.demo.repository.AssignmentEvaluationRepository;
+import com.example.demo.entity.AssignmentEvaluation;
+import com.example.demo.service.AssignmentEvaluationService;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 
-public interface AssignmentEvaluationService
+
+@Service
+public class AssignmentEvaluationServiceImpl implements AssignmentEvaluationService{
+
+@Autowired
+AssignmentEvaluationRepository gr;
+public AssignmentEvaluation createGarage(Garage gar){
+
+    return gr.save(gar);
+}
 
 
-{
-    public AssignmentEvaluationRecord evaluateAssignment(EvaluateAssignment req);
-    public   getEvaluationsByAssignment(GetEvaluationsByAssignment req);
-    public getAIIEvaluations()
 }
