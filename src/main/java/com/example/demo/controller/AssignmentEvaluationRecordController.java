9ea3471a56fc2ef899ef3c049f3b0ccb5fba97a9
bala-amypo <bala@amypo.in>
2025-12-19 10:21:53 +0000

    User commit

--- src/main/java/com/example/demo/controller/AssignmentEvaluationRecordController.java
+++ src/main/java/com/example/demo/controller/AssignmentEvaluationRecordController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
-public class AssignmentEvaluationRecodeController{
+public class AssignmentEvaluationRecordController{
     @Autowired
     private AssignmentEvaluationRecordService a;
     @PostMapping("/AssignmentEvaluationRecord")
