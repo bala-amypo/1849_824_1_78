189c8e148927e14af5c9c0e8e16dbefe539b4a80
bala-amypo <bala@amypo.in>
2025-12-19 13:20:05 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/TaskRecordServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/TaskRecordServiceImpl.java
@@ -1,20 +1,19 @@
 package com.example.demo.service.impl;
+
 import com.example.demo.repository.TaskRecordRepository;
 import com.example.demo.model.TaskRecord;
 import com.example.demo.service.TaskRecordService;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 
-
 @Service
-public class TaskRecordServiceImpl implements TaskRecordService{
+public class TaskRecordServiceImpl implements TaskRecordService {
 
-@Autowired
-TaskRecordRepository trr;
-public  TaskRecord createTaskRecord(TaskRecord tr){
+    @Autowired
+    private TaskRecordRepository trr;
 
-    return trr.save(tr);
-}
-
-
+    @Override
+    public TaskRecord createTaskRecord(TaskRecord tr) {
+        return trr.save(tr);
+    }
 }
