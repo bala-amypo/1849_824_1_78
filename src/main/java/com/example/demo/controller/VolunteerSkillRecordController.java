71123790e43e641420a778aaeb18f52e384a536d
bala-amypo <bala@amypo.in>
2025-12-19 09:43:29 +0000

    User commit

--- src/main/java/com/example/demo/controller/VolunteerSkillRecordController.java
+++ src/main/java/com/example/demo/controller/VolunteerSkillRecordController.java
@@ -1,16 +1,16 @@
 package com.example.demo.controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
-import com.example.demo.model.VolunteerSkill;
-import com.example.demo.service.VolunteerSkillService;
+import com.example.demo.model.VolunteerSkillRecord;
+import com.example.demo.service.VolunteerSkillRecordService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 public class VolunteerSkillRecordController{
     @Autowired
-    private VolunteerSkillService vo;
-    @PostMapping("/VolunteerProfile")
+    private VolunteerSkillRecordService vo;
+    @PostMapping("/VolunteerSkillRecord")
     public VolunteerSkillRecord addVolunteerSkill(@RequestBody VolunteerSkillRecord vs){
         return vo.createVolunteerSkill(vs);
     }
