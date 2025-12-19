0ca47598866b3e9d858d598dec18d1d2109c9327
bala-amypo <bala@amypo.in>
2025-12-19 10:19:30 +0000

    User commit

--- src/main/java/com/example/demo/controller/TaskAssignmentRecordController.java
+++ src/main/java/com/example/demo/controller/TaskAssignmentRecordController.java
@@ -1,17 +1,17 @@
 package com.example.demo.controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
-import com.example.demo.model.TaskAssignment;
-import com.example.demo.service.TaskAssignmentService;
+import com.example.demo.model.TaskAssignmentRecord;
+import com.example.demo.service.TaskAssignmentRecordService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
-public class TaskAssignmentController{
+public class TaskAssignmentRecordController{
     @Autowired
     private TaskAssignmentRecordService t;
     @PostMapping("/TaskAssignmentRecord")
-    public TaskAssignmentRecord addTaskAssignment(@RequestBody TaskAssignmentRecord ta){
+    public TaskAssignmentRecord addTaskAssignmentRecord(@RequestBody TaskAssignmentRecord ta){
         return t.createTaskAssignmentRecord(ta);
     }
 }
