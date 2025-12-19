1d1c2cd61455dad8604892c91b87c205d880ddce
bala-amypo <bala@amypo.in>
2025-12-19 08:09:56 +0000

    User commit

--- src/main/java/com/example/demo/service/TaskRecordService.java
+++ src/main/java/com/example/demo/service/TaskRecordService.java
@@ -1,9 +1,9 @@
 package com.example.demo.service;
 import com.example.demo.entity.TaskAssignment;
 
-public interface   TaskAssignmentService
+public interface   TaskRecordService
 {
-    TaskAssignmentServices createTaskAssignment(TaskAssignment ts);
+    TaskRecordServices createTaskAssignment(TaskAssignment ts);
 
 
 }
