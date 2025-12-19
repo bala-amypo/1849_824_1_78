684cfc30f783395fe4ae4f8b7d226d252c9bc8d8
bala-amypo <bala@amypo.in>
2025-12-19 13:15:42 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/AssignmentEvaluationRecordServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/AssignmentEvaluationRecordServiceImpl.java
@@ -1,20 +1,21 @@
 package com.example.demo.service.impl;
-import com.example.demo.repository.AssignmentEvaluationRepository;
-import com.example.demo.model.AssignmentEvaluation;
-import com.example.demo.service.AssignmentEvaluationRecordService;
-import org.springframework.stereotype.Service;
-import org.springframework.beans.factory.annotation.Autowired;
 
+import com.example.demo.model.AssignmentEvaluationRecord;
+import com.example.demo.repository.AssignmentEvaluationRecordRepository;
+import com.example.demo.service.AssignmentEvaluationRecordService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Service
-public class AssignmentEvaluationRecordServiceImpl implements AssignmentEvaluationRecordService{
+public class AssignmentEvaluationRecordServiceImpl
+        implements AssignmentEvaluationRecordService {
 
-@Autowired
-AssignmentEvaluationRecordRepository aer;
-public AssignmentEvaluationRecord createAssignmentEvaluationRecord(AssignmentEvaluationRecord ae){
+    @Autowired
+    private AssignmentEvaluationRecordRepository aer;
 
-    return aer.save(ae);
-}
-
-
+    @Override
+    public AssignmentEvaluationRecord createAssignmentEvaluationRecord(
+            AssignmentEvaluationRecord ae) {
+        return aer.save(ae);
+    }
 }
