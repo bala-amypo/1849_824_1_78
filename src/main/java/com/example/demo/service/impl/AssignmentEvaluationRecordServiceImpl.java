ec9c71c6ffab78d53019f98572a1cb7bdc8ae34d
bala-amypo <bala@amypo.in>
2025-12-19 13:00:42 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/AssignmentEvaluationRecordServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/AssignmentEvaluationRecordServiceImpl.java
@@ -1,13 +1,13 @@
 package com.example.demo.service.impl;
 import com.example.demo.repository.AssignmentEvaluationRepository;
 import com.example.demo.model.AssignmentEvaluation;
-import com.example.demo.service.AssignmentEvaluationService;
+import com.example.demo.service.AssignmentEvaluationRecordService;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 
 
 @Service
-public class AssignmentEvaluationServiceImpl implements AssignmentEvaluationService{
+public class AssignmentEvaluationRecordServiceImpl implements AssignmentEvaluationService{
 
 @Autowired
 AssignmentEvaluationRepository aer;
