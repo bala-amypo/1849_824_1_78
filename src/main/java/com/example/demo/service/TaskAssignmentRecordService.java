db5bc4b306c1e9a684c94a784d4b52f259cabf4a
bala-amypo <bala@amypo.in>
2025-12-19 10:39:34 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/TaskAssignmentRecordService.java
@@ -0,0 +1,9 @@
+package com.example.demo.service;
+import com.example.demo.model.TaskAssignmentRecord;
+
+public interface   TaskAssignmentRecordService
+{
+    TaskAssignmentRecordServices createTaskAssignmentRecord(TaskAssignmentRecord ts);
+
+
+}
--- src/main/java/com/example/demo/service/TaskAssignmentService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-import com.example.demo.model.TaskAssignment;
-
-public interface   TaskAssignmentService
-{
-    TaskAssignmentServices createTaskAssignment(TaskAssignment ts);
-
-
-}
