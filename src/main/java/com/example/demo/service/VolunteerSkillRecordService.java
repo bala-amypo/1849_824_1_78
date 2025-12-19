ed16dc95efafc6651d4338e7f7f6d2d5d188ad84
bala-amypo <bala@amypo.in>
2025-12-19 09:45:57 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/VolunteerSkillRecordService.java
@@ -0,0 +1,9 @@
+package com.example.demo.service;
+import com.example.demo.entity.VolunteerSkill;
+
+public interface   VolunteerSkillRecordService
+{
+    VolunteerSkillRecordServices createVolunteerSkill(VolunteerSkill vs);
+
+
+}
