8925a09cf19052fd87b1ad66db5d8f8416687854
bala-amypo <bala@amypo.in>
2025-12-19 10:38:34 +0000

    User commit

--- src/main/java/com/example/demo/service/AssignmentEvaluationService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-import com.example.demo.model.AssignmentEvaluation;
-
-public interface  AssignmentEvaluationService
-{
-    AssignmentEvaluation createAssignmentEvaluation(AssignmentEvaluation as);
-
-
-}
