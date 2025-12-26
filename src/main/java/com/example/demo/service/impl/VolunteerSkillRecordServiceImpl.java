7df5c7c1bab0ad9c004776147bfff0601efe1afb
bala-amypo <bala@amypo.in>
2025-12-26 04:41:33 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/VolunteerSkillRecordServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/VolunteerSkillRecordServiceImpl.java
@@ -3,22 +3,37 @@ package com.example.demo.service.impl;
 import com.example.demo.model.VolunteerSkillRecord;
 import com.example.demo.repository.VolunteerSkillRecordRepository;
 import com.example.demo.service.VolunteerSkillRecordService;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 @Service
 public class VolunteerSkillRecordServiceImpl
         implements VolunteerSkillRecordService {
 
     @Autowired
-    private VolunteerSkillRecordRepository vsr;
+    private VolunteerSkillRecordRepository repo;
 
     @Override
-    public VolunteerSkillRecord createVolunteerSkillRecord(
-            VolunteerSkillRecord vs) {
-        return vsr.save(vs);
+    public VolunteerSkillRecord addOrUpdateSkill(
+            VolunteerSkillRecord skill) {
+        return repo.save(skill);
     }
-    public VolunteerSkillRecord getSkillById(Long id){
-        return vsr.findById(id).get();
+
+    @Override
+    public VolunteerSkillRecord getSkillById(Long id) {
+        return repo.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<VolunteerSkillRecord> getSkillsByVolunteer(Long volunteerId) {
+        return repo.findByVolunteerId(volunteerId);
+    }
+
+    @Override
+    public List<VolunteerSkillRecord> getAllSkills() {
+        return repo.findAll();
     }
 }
