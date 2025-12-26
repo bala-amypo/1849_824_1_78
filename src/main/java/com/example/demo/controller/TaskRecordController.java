bc15369213d0e47567fb9129115abaac26ad1e10
bala-amypo <bala@amypo.in>
2025-12-26 04:29:14 +0000

    User commit

--- src/main/java/com/example/demo/controller/TaskRecordController.java
+++ src/main/java/com/example/demo/controller/TaskRecordController.java
@@ -13,22 +13,19 @@ public class TaskRecordController {
 
     @Autowired
     private TaskRecordService taskRecordService;
-
-    // 1. Create task
+ 
     @PostMapping
     public TaskRecord createTask(@RequestBody TaskRecord taskRecord) {
         return taskRecordService.createTask(taskRecord);
     }
-
-    // 2. Update task
+ 
     @PutMapping("/{id}")
     public TaskRecord updateTask(
             @PathVariable Long id,
             @RequestBody TaskRecord updated) {
         return taskRecordService.updateTask(id, updated);
     }
-
-    // 3. Get open tasks
+ 
     @GetMapping("/open")
     public List<TaskRecord> getOpenTasks() {
         return taskRecordService.getOpenTasks();
