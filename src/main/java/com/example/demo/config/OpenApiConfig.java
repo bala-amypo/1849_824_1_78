7fb6c936f203a6c3f21f9ce9ffdb52c84de100b9
bala-amypo <bala@amypo.in>
2025-12-28 14:20:20 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,58 +1,21 @@
-// package com.example.demo.config;
-
-// import io.swagger.v3.oas.models.OpenAPI;
-// import io.swagger.v3.oas.models.servers.Server;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import java.util.List;
-
-// @Configuration
-// public class OpenApiConfig{
-
-//     @Bean
-//     public OpenAPI customOpenAPI() {
-//         return new OpenAPI()
-//                 // You need to change the port as per your server
-//                 .servers(List.of(
-//                         new Server().url("https://9053.32procr.amypo.ai/")
-//                 ));
-//         }
-// }
 package com.example.demo.config;
 
-import java.util.List;
-
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
 
 @Configuration
-public class SwaggerConfig {
+public class OpenApiConfig{
 
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-
+                // You need to change the port as per your server
                 .servers(List.of(
-                        new Server().url("https://9211.408procr.amypo.ai/")
-                ))
-         
-                .components(new Components()
-                        .addSecuritySchemes("bearerAuth",
-                                new SecurityScheme()
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")
-                        )
-                )
-
-                .addSecurityItem(
-                        new SecurityRequirement().addList("bearerAuth")
-                );
-    }
+                        new Server().url("https://9053.32procr.amypo.ai/")
+                ));
+        }
 }
+ 
