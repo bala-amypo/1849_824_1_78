b27362d19e1402ae44bdeafd160515e2b31f6471
bala-amypo <bala@amypo.in>
2025-12-29 04:37:56 +0000

    User commit

--- src/main/java/com/example/demo/dto/EvaluationRequest.java
+++ src/main/java/com/example/demo/dto/EvaluationRequest.java
@@ -1,51 +1,19 @@
-// package com.example.demo.dto;
-
-// public class EvaluationRequest {
-
-//     private Long assignmentId;
-//     private Integer rating;
-//     private String comments;
-
-//     public EvaluationRequest() {
-//     }
-
-//     public EvaluationRequest(Long assignmentId, Integer rating, String comments) {
-//         this.assignmentId = assignmentId;
-//         this.rating = rating;
-//         this.comments = comments;
-//     }
-
-//     public Long getAssignmentId() {
-//         return assignmentId;
-//     }
-
-//     public void setAssignmentId(Long assignmentId) {
-//         this.assignmentId = assignmentId;
-//     }
-
-//     public Integer getRating() {
-//         return rating;
-//     }
-    
-//     public void setRating(Integer rating) {
-//         this.rating = rating;
-//     }
-
-//     public String getComments() {
-//         return comments;
-//     }
-    
-//     public void setComments(String comments) {
-//         this.comments = comments;
-//     }
-// }
 package com.example.demo.dto;
 
 public class EvaluationRequest {
 
     private Long assignmentId;
-    private int rating;
+    private Integer rating;
     private String comments;
+
+    public EvaluationRequest() {
+    }
+
+    public EvaluationRequest(Long assignmentId, Integer rating, String comments) {
+        this.assignmentId = assignmentId;
+        this.rating = rating;
+        this.comments = comments;
+    }
 
     public Long getAssignmentId() {
         return assignmentId;
@@ -55,19 +23,20 @@ public class EvaluationRequest {
         this.assignmentId = assignmentId;
     }
 
-    public int getRating() {
+    public Integer getRating() {
         return rating;
     }
-
-    public void setRating(int rating) {
+    
+    public void setRating(Integer rating) {
         this.rating = rating;
     }
 
     public String getComments() {
         return comments;
     }
-
+    
     public void setComments(String comments) {
         this.comments = comments;
     }
 }
+ 
